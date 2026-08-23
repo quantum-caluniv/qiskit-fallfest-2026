386c8dca0161d6d8d9dd0f4b199cf77a99c7e81b
Suchetana Das <suchetana.das1140@gmail.com>
2026-08-23 15:43:46 +0530

    Updating faq

--- content/faq_data.tsx
+++ content/faq_data.tsx
@@ -2,8 +2,8 @@ export const faqdata = [
 
     {
         id:1,
-        q:"Question?",
-        ans:"Answer"
+        q:"When is the event going to happen?",
+        ans:"8 and 9 th of October"
     },
     {
         id:2,
